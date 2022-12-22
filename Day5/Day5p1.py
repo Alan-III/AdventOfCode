1ce41023ab89c20123f195fc4a4b7cc78d467a8b
Alan-III <alexander_grt@hotmail.com>
2022-12-22 20:29:24 +0200

    first commit

---
+++ Day5/Day5p1.py
@@ -0,0 +1,34 @@
+contents = []
+readable = True
+f = open("E:\\repos\AdventOfCode\Day5\inputpuzzle1.txt", "r")
+while readable:
+    line = f.readline()
+    if line == '':
+        readable = False
+    contents.append(line)
+f.close()        
+stacks = { 
+    '1': ['G','F','V','H','P','S'],
+    '2': ['G','J','F','B','C','D','Z','M'],
+    '3': ['G','M','L','J','N'],
+    '4': ['N','G','Z','V','D','W','P'],
+    '5': ['V','R','C','B'],
+    '6': ['V','R','S','M','P','W','L','Z'],
+    '7': ['T','H','P'],
+    '8': ['Q','R','S','N','C','H','Z','V'],
+    '9': ['F','L','G','P','V','Q','J']
+}
+x = []
+i=j=0
+while i < len(contents)-1:
+    x = contents[i].split(' ')
+    x[5] = x[5].strip()
+    while j < int(x[1]):
+        stacks[x[5]].append(stacks[x[3]][len(stacks[x[3]])-1])
+        del stacks[x[3]][len(stacks[x[3]])-1]
+        j += 1
+    x.clear()
+    i += 1
+    j = 0
+
+print(stacks)
